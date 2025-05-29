fab3d2991ddefdabf219e6f5ec25188ebbb2dbd3
Dayvinho <dayvialighieri@gmail.com>
2025-05-28 21:12:19 -0300

    Add files via upload

---
+++ Projetos/treinamento.js
@@ -0,0 +1,19 @@
+//equação quadratica: x^2−5x+6=0
+
+//Formula quadratica morderna: x= (-b +- √b^2 -4.a.c)/a.2
+
+//operação de soma da primera raiz
+
+//raiz quadrada
+let q = (5*5) - (4*1*6)
+//junção de - com - da +
+let jun = -(-5)
+//multiplicação de a.2
+let m = 1*2
+//valor da primeira raiz
+let r1 = (jun + q)/m
+//valor da segunda raiz
+let r2 = (jun - q)/m
+
+console.log(" o valor da primeira raiz é " + r1 + " o valor da segunda raiz é " + r2 )
+
